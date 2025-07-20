1360d49cd95be403fe9d838d0ee3d164d99e4228
Sumanth <sumanthomgowda@gmail.com>
2025-07-20 04:01:23 +0000

    finetuning resnet1k to 10

--- shared_utilities.py
+++ shared_utilities.py
@@ -385,6 +385,94 @@ class CustomDataModule(L.LightningDataModule):
         )
         return test_loader
 
+class Cifar10DataModule(L.LightningDataModule):
+    def __init__(
+        self,
+        data_path="./",
+        batch_size=64,
+        height_width=None,
+        num_workers=0,
+        train_transform=None,
+        test_transform=None,
+    ):
+        super().__init__()
+        self.batch_size = batch_size
+        self.data_path = data_path
+        self.num_workers = num_workers
+        self.train_transform = train_transform
+        self.test_transform = test_transform
+        self.height_width = height_width
+
+    def prepare_data(self):
+        datasets.CIFAR10(root=self.data_path, download=True)
+
+        if self.height_width is None:
+            self.height_width = (32, 32)
+
+        if self.train_transform is None:
+            self.train_transform = transforms.Compose(
+                [
+                    transforms.Resize(self.height_width),
+                    transforms.ToTensor(),
+                ]
+            )
+
+        if self.test_transform is None:
+            self.test_transform = transforms.Compose(
+                [
+                    transforms.Resize(self.height_width),
+                    transforms.ToTensor(),
+                ]
+            )
+
+        return
+
+    def setup(self, stage=None):
+        train = datasets.CIFAR10(
+            root=self.data_path,
+            train=True,
+            transform=self.train_transform,
+            download=False,
+        )
+
+        self.test = datasets.CIFAR10(
+            root=self.data_path,
+            train=False,
+            transform=self.test_transform,
+            download=False,
+        )
+
+        self.train, self.valid = random_split(train, lengths=[45000, 5000])
+
+    def train_dataloader(self):
+        train_loader = DataLoader(
+            dataset=self.train,
+            batch_size=self.batch_size,
+            drop_last=True,
+            shuffle=True,
+            num_workers=self.num_workers,
+        )
+        return train_loader
+
+    def val_dataloader(self):
+        valid_loader = DataLoader(
+            dataset=self.valid,
+            batch_size=self.batch_size,
+            drop_last=False,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
+        return valid_loader
+
+    def test_dataloader(self):
+        test_loader = DataLoader(
+            dataset=self.test,
+            batch_size=self.batch_size,
+            drop_last=False,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
+        return test_loader
 
 def plot_loss_and_acc(
         log_dir, loss_ylim=(0.0, 0.9), acc_ylim=(0.7, 1.0), save_loss=None, save_acc=None
